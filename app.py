6f5bcda9d07dd3414a8a9f7e5c72f729299c2bcb
EKaizen2 <55081970+EKaizen2@users.noreply.github.com>
2022-04-11 21:50:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def main():
     # front end elements of the web page
     html_temp = """ 
     <div style ="background-color:#002E6D;padding:20px;font-weight:15px"> 
-    <h1 style ="color:white;text-align:center;">Ephraim Adongo New Article Recommender</h1> 
+    <h1 style ="color:white;text-align:center;">Ephraim Adongo News Article Recommender</h1> 
     </div> 
     """
 
@@ -31,6 +31,7 @@ def main():
     story = st.selectbox(
          'Select one article to get a recommendation of similar articles: ',
          ('‘The Voice’ Blind Auditions Make History With First Trans Contestant', '‘RuPaul’s Drag Race All Stars 3’ Episode 7 Recap: We Can Never Go Back To Before', 'Would Dr. King Take A Knee? 6 Ways His Radical Spirit Lives On Today', 'Why Would DeVos Cancel A Student Loan Rule That Works?', 'Veterans Affairs Secretary David Shulkin Ousted From White House'))
+    selectedheadline = story
     if story == '‘The Voice’ Blind Auditions Make History With First Trans Contestant':
         story = 1
     elif story == "‘RuPaul’s Drag Race All Stars 3’ Episode 7 Recap: We Can Never Go Back To Before":
@@ -49,12 +50,12 @@ def main():
 
     result = ""
     
-    if st.button("Predict"):
+    if st.button("Recommend"):
       def tfidf_based_model(row_index, num_similar_items):
         couple_dist = pairwise_distances(tfidf_headline_features,tfidf_headline_features[row_index])
         indices = np.argsort(couple_dist.ravel())[0:num_similar_items]
-        df = pd.DataFrame({'publish_date': news_articles['date'][indices].values,
-                   'headline':news_articles['headline'][indices].values,
+        df = pd.DataFrame({'Publish_date': news_articles['date'][indices].values,
+                   'Headline':news_articles['headline'][indices].values,
                     'Euclidean similarity with the queried article': couple_dist[indices].ravel()})
         print("="*30,"Queried article details","="*30)
         print('headline : ',news_articles['headline'][indices[0]])
@@ -63,7 +64,9 @@ def main():
         #return df.iloc[1:,1]
         return df.iloc[1:,]
       result = tfidf_based_model(story, 11)
+      st.write("============================== Queried article details ==============================")
+      st.write("Selected headline :" +selectedheadline)
+      st.write("========================= Recommended articles :  =======================")
       st.write(result)
-
 if __name__ == '__main__':
     main()
